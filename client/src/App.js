cf41bc515d6d00f6ab16587ff8b268b64e8980a1
unknown <sebastiansryan@gmail.com>
2019-09-08 16:48:05 -0700

    added gradient background

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        Porter Robinson
-      </header>
+      <Header />
     </div>
   );
 }
